d6f6da2a37373cd76706d84fe4a4335a4bf47fb3
Tristan Barrett <1006459@student.sae.edu.au>
2017-02-15 15:36:55 +1000

    player can move and shoot using controller or keys
    shooting aimed and triggered by selecting a direction with arrow keys or right stick. camera follows player super basic

---
+++ Assets/BulletController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+    public DeadZone deadZone;
+
+    public float speed = 50f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        float shootX = Input.GetAxis("xShoot");
+        float shootY = Input.GetAxis("yShoot");
+
+        if (shootX > deadZone.posZone)
+        {
+            shootX = speed;
+        }
+
+        if (shootX < deadZone.negZone)
+        {
+            shootX = -speed;
+        }
+
+        if (shootY > deadZone.posZone)
+        {
+            shootY = speed;
+        }
+
+        if (shootY < deadZone.negZone)
+        {
+            shootY= -speed;
+        }
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2 (shootX, shootY);
+
+        StartCoroutine(despawnTime());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+    }
+
+    IEnumerator despawnTime()
+    {
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin = -29, xMax = 29;
+    public float yMin = -14, yMax = 14;
+}
+
+[System.Serializable]
+public class DeadZone
+{
+    public float posZone = 0.2f;
+    public float negZone = -0.2f;
+}
+
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Boundary boundary;
+
+    public DeadZone deadZone;
+
+    public Rigidbody2D rb;
+
+    public float speed = 30f;
+
+    public Transform playerBullet;
+    public float fireRate;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Assign rigidbody
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("xMove");
+        float moveY = Input.GetAxis("yMove");
+
+        //Move the player
+        Vector2 movement = new Vector2(moveX, moveY);
+        rb.velocity = movement * speed;
+
+        float shootX = Input.GetAxis("xShoot");
+        float shootY = Input.GetAxis("yShoot");
+
+        if (shootX > deadZone.posZone || shootX < deadZone.negZone)
+        {
+            Instantiate(playerBullet, transform.position, playerBullet.rotation);
+        }
+
+        if (shootY > deadZone.posZone || shootY < deadZone.negZone)
+        {
+            Instantiate(playerBullet, transform.position, playerBullet.rotation);
+        }
+    }
+}
