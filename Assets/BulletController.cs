8fe9061e2f4ea83bd2913fb29488b55f11b4215d
Tristan Barrett <1006459@student.sae.edu.au>
2017-02-16 08:38:07 +1000

    minor updates and changes

--- Assets/BulletController.cs
+++ Assets/BulletController.cs
@@ -46,7 +46,7 @@ public class BulletController : MonoBehaviour {
 
     IEnumerator despawnTime()
     {
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(2);
         Destroy(gameObject);
     }
 }
